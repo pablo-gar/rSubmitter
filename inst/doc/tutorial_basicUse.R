f62006fb6c99298671beadb8151c912a87298feb
Pablo E Garcia-Nieto <paedugar@gmail.com>
2018-04-23 18:55:08 -0700

    Expand vigenettes

--- inst/doc/tutorial_basicUse.R
+++ inst/doc/tutorial_basicUse.R
@@ -1,1 +1,11 @@
+## ----eval=FALSE----------------------------------------------------------
+#  library("rSubmmiter")
+#  myJob <- Job$new(commandVector = c("echo hola world!", "sleep 30"))
+
+## ----eval=FALSE----------------------------------------------------------
+#  myJob$submit()
+#  myJob$wait()
+#   2018-04-23 18:22:35 --- Cluster Status |  PENDING = 1 |
+#   2018-04-23 18:22:40 --- Cluster Status |  RUNNING = 1 |
+#   2018-04-23 18:23:30 --- Cluster Status |  COMPLETED = 1 |
 
