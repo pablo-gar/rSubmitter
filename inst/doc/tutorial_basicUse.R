f90c82472f65156967c588248e455456940c8644
Pablo E Garcia-Nieto <paedugar@gmail.com>
2018-05-04 16:09:57 -0700

    Update documentation

--- inst/doc/tutorial_basicUse.R
+++ inst/doc/tutorial_basicUse.R
@@ -62,24 +62,3 @@
 ## ----eval=FALSE----------------------------------------------------------
 #  ?superApply
 
-## ----eval=FALSE----------------------------------------------------------
-#  myJob$submit()
-#  myJob$wait()
-#    2018-04-23 18:22:35 --- Cluster Status |  PENDING = 1 |
-#    2018-04-23 18:22:40 --- Cluster Status |  RUNNING = 1 |
-#    2018-04-23 18:23:30 --- Cluster Status |  COMPLETED = 1 |
-
-## ----eval=FALSE----------------------------------------------------------
-#  myJob$submit()$wait()
-
-## ----eval=FALSE----------------------------------------------------------
-#  myJob$submit()
-#  myJob$cancel()
-#    2018-04-23 19:17:30 Cancelling 1 job(s)
-#    2018-04-23 19:17:32 Finished sending cancel signal
-
-## ----eval=FALSE----------------------------------------------------------
-#  myJob <- Job$new(commandVector = c("echo hola world!", "sleep 30"), jobName = "testJob", outDir = "/tmp/")
-#  myJob$submit()$wait()
-#  myJob$clean()
-
