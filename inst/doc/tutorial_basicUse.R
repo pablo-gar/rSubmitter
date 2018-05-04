87c29f728d05726682992770d7f3bc9962b2b28e
Pablo E Garcia-Nieto <paedugar@gmail.com>
2018-05-04 15:40:08 -0700

    Update documentation

--- inst/doc/tutorial_basicUse.R
+++ inst/doc/tutorial_basicUse.R
@@ -27,6 +27,30 @@
 #  #   2018-04-25 17:50:50 --- Cluster Status |  COMPLETED = 2 |
 
 ## ----eval=FALSE----------------------------------------------------------
+#  x <- lapply(1:4, as.character)
+
+## ----eval=FALSE----------------------------------------------------------
+#  x <- list()
+#  for(i in 1:4)
+#      x[[i]] <- as.character(i)
+
+## ----eval=FALSE----------------------------------------------------------
+#  myFun <- function(x) {
+#      return(rep(x, 3))
+#  }
+#  
+#  dir.create("~/testSap")
+#  x <- superApply(1:100, myFun, tasks = 4)
+#  #   2018-05-04 15:29:34 Partitioning function calls
+#  #   2018-05-04 15:29:35 Submmiting parallel Jobs
+#  #   2018-05-04 15:29:40 --- Cluster Status |  PENDING = 4 |
+#  #   2018-05-04 15:29:45 --- Cluster Status |  COMPLETED = 4 |
+#  #   2018-05-04 15:29:45 Merging parellel results
+#  #   2018-05-04 15:29:45 Merge done
+#  #   2018-05-04 15:29:46 Cleaning partitioned data
+#  #   2018-05-04 15:29:47 Cleaning done
+
+## ----eval=FALSE----------------------------------------------------------
 #  myJob$submit()
 #  myJob$wait()
 #    2018-04-23 18:22:35 --- Cluster Status |  PENDING = 1 |
